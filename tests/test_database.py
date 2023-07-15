e5b88b148f1677c14ecc3a449345e975d992cf65
Владимир Войтенко <vladimirdev635@gmail.com>
2023-07-15 18:52:23 +0500

    add-testes

---
+++ tests/test_database.py
@@ -0,0 +1,9 @@
+from iorm import Database
+
+
+def test_database() -> None:
+    class MainDatabase(Database):
+        pass
+
+    db = MainDatabase("main")
+    assert db.get_sql_hash() is not None
