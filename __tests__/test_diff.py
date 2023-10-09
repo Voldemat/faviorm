2f66637f110bc01149ac8a65229847c3ff679d37
ukrainskykirill <ukrainsky.kirill@gmail.com>
2023-10-10 01:01:37 +0300

    faviorm:add_some_new_types

--- __tests__/test_diff.py
+++ __tests__/test_diff.py
@@ -364,10 +364,10 @@ def test_diff_change_enum_type_of_column() -> None:
 
 def test_change_timestamp_default_value_of_column() -> None:
     class UsersTable(faviorm.Table):
-        exp_at = faviorm.Column("exp_at", faviorm.TIMESTAMP(), True, "NOW()")
+        exp_at = faviorm.Column("exp_at", faviorm.TIMESTAMP(), True, "now")
 
     class Users2Table(faviorm.Table):
-        exp_at = faviorm.Column("exp_at", faviorm.TIMESTAMP(), True, "TODAY()")
+        exp_at = faviorm.Column("exp_at", faviorm.TIMESTAMP(), True, "today")
 
     class D1(faviorm.Database):
         users = UsersTable("users")
@@ -388,6 +388,159 @@ def test_change_timestamp_default_value_of_column() -> None:
                                 "default": {
                                     "from": UsersTable.exp_at.get_default(),
                                     "to": Users2Table.exp_at.get_default(),
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }, diff
+
+
+def test_numeric_column() -> None:
+    class UsersTable(faviorm.Table):
+        col = faviorm.Column("col", faviorm.NUMERIC(2), False, 222)
+
+    class Users2Table(faviorm.Table):
+        col = faviorm.Column("col", faviorm.REAL(), False)
+
+    class D1(faviorm.Database):
+        users = UsersTable("users")
+
+    class D2(faviorm.Database):
+        users = Users2Table("users")
+
+    d1 = D1()
+    d2 = D2()
+    diff = faviorm.diff(d1, d2, hasher=hasher)
+    assert diff == {
+        "tables": {
+            "changed": {
+                "users": {
+                    "columns": {
+                        "changed": {
+                            "col": {
+                                "type": {
+                                    "from": UsersTable.col.get_type(),
+                                    "to": Users2Table.col.get_type(),
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }, diff
+
+
+def test_float_column() -> None:
+    class UsersTable(faviorm.Table):
+        num_col = faviorm.Column(
+            "num_col",
+            faviorm.FLOAT(2),
+            False,
+        )
+
+    class Users2Table(faviorm.Table):
+        num_col = faviorm.Column("num_col", faviorm.DOUBLE_PRECISION(), False)
+
+    class D1(faviorm.Database):
+        users = UsersTable("users")
+
+    class D2(faviorm.Database):
+        users = Users2Table("users")
+
+    d1 = D1()
+    d2 = D2()
+    diff = faviorm.diff(d1, d2, hasher=hasher)
+    assert diff == {
+        "tables": {
+            "changed": {
+                "users": {
+                    "columns": {
+                        "changed": {
+                            "num_col": {
+                                "type": {
+                                    "from": UsersTable.num_col.get_type(),
+                                    "to": Users2Table.num_col.get_type(),
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }, diff
+
+
+def test_column_money_default() -> None:
+    class UsersTable(faviorm.Table):
+        salary = faviorm.Column("salary", faviorm.MONEY(2), False, "$1000")
+
+    class Users2Table(faviorm.Table):
+        salary = faviorm.Column("salary", faviorm.MONEY(2), False, "$2000")
+
+    class D1(faviorm.Database):
+        users = UsersTable("users")
+
+    class D2(faviorm.Database):
+        users = Users2Table("users")
+
+    d1 = D1()
+    d2 = D2()
+    diff = faviorm.diff(d1, d2, hasher=hasher)
+    assert diff == {
+        "tables": {
+            "changed": {
+                "users": {
+                    "columns": {
+                        "changed": {
+                            "salary": {
+                                "default": {
+                                    "from": UsersTable.salary.get_default(),
+                                    "to": Users2Table.salary.get_default(),
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }, diff
+
+
+def test_time_and_interal_column() -> None:
+    class UsersTable(faviorm.Table):
+        time = faviorm.Column(
+            "time",
+            faviorm.TIME(2),
+            False,
+        )
+
+    class Users2Table(faviorm.Table):
+        time = faviorm.Column("time", faviorm.INTERVAL(2), False)
+
+    class D1(faviorm.Database):
+        users = UsersTable("users")
+
+    class D2(faviorm.Database):
+        users = Users2Table("users")
+
+    d1 = D1()
+    d2 = D2()
+    diff = faviorm.diff(d1, d2, hasher=hasher)
+    print(diff)
+    assert diff == {
+        "tables": {
+            "changed": {
+                "users": {
+                    "columns": {
+                        "changed": {
+                            "time": {
+                                "type": {
+                                    "from": UsersTable.time.get_type(),
+                                    "to": Users2Table.time.get_type(),
                                 }
                             }
                         }
